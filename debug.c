757c8aec155249a0b3bf1d748f4055657ee3f29f
Qsx <sixiang.qiu@stud.th-luebeck.de>
2021-12-26 21:59:03 +0800

    debug.c

---
+++ debug.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    int value_int, array_int[3];
+    float value_float;
+    char* value_char_pointer;
+
+    value_int = 1048576;
+    value_float = 2.0;
+    value_char_pointer = "Hello World";
+    array_int[0] = 379;
+    array_int[1] = 94;
+
+    return 0;
+}
