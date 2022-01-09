7657821e50dc826fbadbef11706f2f55f660ca32
Sixiang Qiu <sixiang.qiu@student.unsw.edu.au>
2022-01-09 21:21:27 +0800

    format

--- Chapter6/exercise6.15.c
+++ Chapter6/exercise6.15.c
@@ -9,8 +9,10 @@ int main(void) {
     printf("Enter a string: ");
     for (i = 0; word[i - 1] != '\n'; i++, length = i - 2)
         scanf("%c", &word[i]);
+
     printf("Reverse string: ");
     for (i = length; i >= 0; i--)
         printf("%c", word[i]);
+        
     return 0;
 }
