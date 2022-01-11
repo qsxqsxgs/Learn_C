8aa5d9b82685622a232e39b95da2cdc3a8c7dcef
Sixiang Qiu <sixiang.qiu@student.unsw.edu.au>
2022-01-11 20:00:26 +0800

    exercise7

---
+++ Chapter7/exercise7.6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+    char ch;
+    char prev = '\0';
+    int  time = 0;
+
+    printf("Enter a text to be analyzed(# to terminate): ");
+    while ((ch = getchar()) != '#') {
+        if (ch == 'i' && prev == 'e')
+            time++;
+        prev = ch;
+    }
+    printf("Number of string \'ei\' is: %d", time);
+
+    return 0;
+}
