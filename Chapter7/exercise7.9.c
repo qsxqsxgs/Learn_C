553b3e6130eff9d709962358f57842fa51a6bd70
Sixiang Qiu <sixiang.qiu@student.unsw.edu.au>
2022-01-13 19:56:36 +0800

    Create exercise7.9.c

---
+++ Chapter7/exercise7.9.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    unsigned int num;
+
+    printf("Enter a positive integer: ");
+    if (scanf("%u", &num) == 1) {
+        for (int i = 1; i <= num; i++)
+            
+    }
+}
