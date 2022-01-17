04ed786f1af3eed596cdfa4e2696f945c91e1faf
Sixiang Qiu <sixiang.qiu@student.unsw.edu.au>
2022-01-17 17:28:22 +0800

    exercise9

--- Chapter7/exercise7.9.c
+++ Chapter7/exercise7.9.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+bool isPrime(unsigned int num);
 
 int main(void) {
     unsigned int num;
 
     printf("Enter a positive integer: ");
-    if (scanf("%u", &num) == 1) {
-        for (int i = 1; i <= num; i++)
-            
-    }
+    // printf("%d", isPrime(3));
+    scanf("%u", &num);
+    for (int i = 1; i <= num; i++)
+        if (isPrime(i))
+            printf("Prime: %d\n", i);
+}
+
+bool isPrime(unsigned int num) {
+    for (int i = 2; i * i <= num; i++)
+        if (num % i == 0)
+            return false;
+    return true;
 }
