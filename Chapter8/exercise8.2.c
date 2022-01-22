7aa33c3b75ebaaf5a8deb340b9fddc49dafd30b8
Sixiang Qiu <sixiang.qiu@student.unsw.edu.au>
2022-01-23 00:27:32 +0800

    exercise2

--- Chapter8/exercise8.2.c
+++ Chapter8/exercise8.2.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
+#define PATTERN 10
 
 int main(void) {
     char ch;
-    int  count;
+    int  count = 0;
 
     while ((ch = getchar()) != EOF) {
-        
+        if (count++ % PATTERN == 0)
+            printf("\n");
+        /* Line counter */
+        if (ch >= '\040')
+            printf("\'%c\'--%3d ", ch, ch);
+        else if (ch == '\n') {
+            printf("\'\\n\'--\\n\n ");
+            count = 0;
+        } else if (ch == '\t')
+            printf("\'\\t\'--\\t ");
+        else
+            printf("\'%c\'--^%c", ch, (ch + 64));
     }
+
+    return 0;
 }
